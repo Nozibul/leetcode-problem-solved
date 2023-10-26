daef203ca9d6a0c5db75a7d71850b5d7ec72d303
Nozibul <nozibulislamspi@gmail.com>
2023-10-26 22:47:43 +0600

    own reducer -Array Reduce Transfomation 2626

---
+++ Array Reduce Transformation-2626-easy.js
@@ -0,0 +1,15 @@
+
+var reduce = function(nums, fn, init) {
+    let acc = init;
+    // if(!acc) acc =  nums[0];
+
+    for(let i = 0; i < nums.length; i++){
+        console.log(nums[i]);
+      acc = fn(acc, nums[i])
+    }
+    return acc ;
+};
+const ownReduce = reduce([1,2,3,4], (acc, cur)=>{
+    return acc + cur ;
+}, 0);
+console.log(ownReduce)
