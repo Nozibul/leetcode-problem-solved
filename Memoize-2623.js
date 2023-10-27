a84a9724885d94a3286a96c731b9f2a8b385306e
Nozibul <nozibulislamspi@gmail.com>
2023-10-27 13:14:51 +0600

    Memoize - 2623

---
+++ Memoize-2623.js
@@ -0,0 +1,20 @@
+// Higher Order Function
+function memoize(fn) {
+    let cache = {};
+    return function (...args) {
+      const argKey = JSON.stringify(args);
+      if ( cache[argKey] !== undefined) return cache[argKey];
+      return cache[argKey] = fn(...args);
+    };
+  }
+  
+  let callCount = 0;
+  const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+  });
+  console.log(memoizedFn([[0,0],[0,0],[]]));
+  console.log(memoizedFn(2, 3)); // 5
+  console.log(memoizedFn(2, 3)); // 5
+  
+  console.log(callCount); // 1
