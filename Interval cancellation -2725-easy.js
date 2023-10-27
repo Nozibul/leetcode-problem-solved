c43b59a7d11e91925666c908bbc6a09d3f93c986
Nozibul <nozibulislamspi@gmail.com>
2023-10-28 00:58:06 +0600

    Interval cancellation 2725

---
+++ Interval cancellation -2725-easy.js
@@ -0,0 +1,33 @@
+var cancellable = function (fn, args, t) {
+  fn(...args); // Call fn immediately
+  const timer = setInterval(() => fn(...args), t);
+  return () => clearInterval(timer);
+  
+};
+
+const result = [];
+const fn = (x) => x * 2;
+const args = [4],
+  t = 35,
+  cancelT = 190;
+const start = performance.now();
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+setTimeout(() => {
+  cancel();
+}, cancelT);
+
+setTimeout(() => {
+  console.log(result); // [
+  //      {"time":0,"returned":8},
+  //      {"time":35,"returned":8},
+  //      {"time":70,"returned":8},
+  //      {"time":105,"returned":8},
+  //      {"time":140,"returned":8},
+  //      {"time":175,"returned":8}
+  //  ]
+}, cancelT + t + 15);
