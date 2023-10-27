5cbf7fa9bc65ca3dc2376a8541dcac48c99fa986
Nozibul <nozibulislamspi@gmail.com>
2023-10-27 21:48:22 +0600

    Add two promises 2723'

--- Add Two Promises-2723-easy.js
+++ Add Two Promises-2723-easy.js
@@ -1,5 +1,4 @@
 var addTwoPromises = async function (promise1, promise2) {
-    console.log(promise1, promise2);
   return Promise.all([promise1, promise2]).then(result =>{
     const sum = result.reduce((total, curValue) => total + curValue, 0);
     return sum;
@@ -8,11 +7,3 @@ var addTwoPromises = async function (promise1, promise2) {
 };
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(3)).then(console.log); // 4
-
-
-
-/*
-Input: 
-promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
-promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60)) 
-*/
