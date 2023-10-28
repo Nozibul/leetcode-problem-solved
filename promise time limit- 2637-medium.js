6c58128ece0f8d595f793d6786544a01f94d547a
Nozibul <nozibulislamspi@gmail.com>
2023-10-28 14:26:54 +0600

    Promise time limit 2637- meduim

---
+++ promise time limit- 2637-medium.js
@@ -0,0 +1,13 @@
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+   return Promise.race([
+    fn(...args),
+    new Promise((_, reject) => {
+        setTimeout(()=> reject("Time Limit Exceeded"), t)
+    })
+   ])
+  };
+};
+
+const limited = timeLimit((t) => new Promise((res) => setTimeout(res, t)), 120);
+limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
