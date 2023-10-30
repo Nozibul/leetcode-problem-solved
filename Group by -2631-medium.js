e399a3058409a9d422cf6e3e5962f46bc8291165
Nozibul <nozibulislamspi@gmail.com>
2023-10-30 10:33:36 +0600

    Group by 2631 -medium

---
+++ Group by -2631-medium.js
@@ -0,0 +1,12 @@
+Array.prototype.groupBy = function (fn) {
+  return this.reduce((acc, curr) => {
+    let key = fn(curr);
+    if (!acc[key]) acc[key] = [];
+    acc[key].push(curr);
+    return acc;
+  }, {});
+};
+
+var arr = [1, 2, 3].groupBy(String);
+
+console.log(arr);
