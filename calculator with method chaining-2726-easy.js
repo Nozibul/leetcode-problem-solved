38dd8a373e0773dd1d1e7cbd64f546420ffe07d5
Nozibul <nozibulislamspi@gmail.com>
2023-10-31 21:44:56 +0600

    calculator with method chaining chaining 2726

---
+++ calculator with method chaining-2726-easy.js
@@ -0,0 +1,39 @@
+class Calculator {
+    constructor(value = 0) {
+        this.value = value;
+    }
+
+    add(value) {
+        this.value += value;
+        return this;
+    }
+
+    subtract(value) {
+        this.value -= value;
+        return this;
+    }
+
+    multiply(value) {
+        this.value *= value;
+        return this;
+    }
+
+    divide(value) {
+        if (value === 0) this.value = "Division by zero is not allowed" ;
+        else this.value /= value;
+        return this;
+    }
+
+    power(value) {
+        this.value **= value;
+        return this;
+    }
+
+    getResult() {
+        return this.value;
+    }
+}
+
+const result = new Calculator(20).divide(0).getResult();
+
+console.log("Result:", result);
