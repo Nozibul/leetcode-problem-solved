bfb655a9390e15725f889bef4c95060c51694491
Nozibul <nozibulislamspi@gmail.com>
2023-11-01 08:41:05 +0600

    Event Emit 2694

---
+++ Event Emitter-2694-medium.js
@@ -0,0 +1,42 @@
+class EventEmitter {
+    constructor() {
+      this.events = {};
+    }
+
+    subscribe(eventName, callback) {
+      if (!this.events[eventName]) {
+        this.events[eventName] = [];
+      }
+  
+      this.events[eventName].push(callback);
+  
+      return {
+        unsubscribe: () => {
+          this.events[eventName] = this.events[eventName].filter(cb => cb !== callback);
+        }
+      };
+    }
+  
+    emit(eventName, args = []) {
+      const callbacks = this.events[eventName] || [];
+      const results = callbacks.map(callback => callback(...args));
+      return results;
+    }
+  }
+  
+  const emitter = new EventEmitter();
+  
+  // Subscribe to the onClick event with onClickCallback
+  function onClickCallback() {
+    return 99;
+  }
+  
+  const sub = emitter.subscribe('onClick', onClickCallback);
+  
+  const results = emitter.emit('onClick');
+  console.log(results); // Output: [99]
+  
+  sub.unsubscribe();
+  const resultsAfterUnsubscribe = emitter.emit('onClick');
+  console.log(resultsAfterUnsubscribe); // Output: []
+  
