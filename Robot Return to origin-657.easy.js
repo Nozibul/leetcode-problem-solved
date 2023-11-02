4440a7149eb9c7eca342b522e5ae1f0b162cc6ea
Nozibul <nozibulislamspi@gmail.com>
2023-11-02 20:36:14 +0600

    Update the code of Pobot return to origin 657

--- Robot Return to origin-657.easy.js
+++ Robot Return to origin-657.easy.js
@@ -2,12 +2,12 @@ var judgeCircle = function(moves) {
     let x = 0 ; 
     let y = 0 ;
     for(let i = 0; i< moves.length; i++){
-        if(moves[i]==="R") return x++ ;
-        else if(moves[i]==="L") return x-- ;
-        else if(moves[i]==="U") return y++ ;
-        else if(moves[i]==="D") return y-- ;
+        if(moves[i]==="R") x++ ;
+        else if(moves[i]==="L") x-- ;
+        else if(moves[i]==="U") y++ ;
+        else if(moves[i]==="D") y-- ;
     }
-    return x === 0 && y === 0 ;
+    return x === 0 && y === 0 ? true : false;
 }
 const result  = judgeCircle("UD");
 console.log(result);
