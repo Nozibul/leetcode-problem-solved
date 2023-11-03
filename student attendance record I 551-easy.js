dac8c451f3c4d86a7f4b3d3abfde04c24303ea83
Nozibul <nozibulislamspi@gmail.com>
2023-11-03 10:56:12 +0600

    student attendance record I - 551

---
+++ student attendance record I 551-easy.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    let absentDays = 0;
+    let lateDays = 0 ;
+    for(let i = 0; i < s.length ; i++){
+        if(s[i] === "A"){
+            absentDays++;
+            if(absentDays >= 2) return false ;
+            lateDays = 0;
+
+        } else if(s[i] === "L"){
+           lateDays++;
+           if(lateDays >= 3) return false ;
+        }else lateDays = 0 ;
+    }
+    return true ;
+};
+const result = checkRecord("LALLL");
+console.log(result);
