d9564209410f3d563437e3d124777a6a25d3e704
Nozibul <nozibulislamspi@gmail.com>
2023-11-03 12:31:35 +0600

    Add string -415

---
+++ Add strinh 415-easy.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        # Convert strings to integers
+        number1 = int(num1)
+        number2 = int(num2)
+        result = number1 + number2
+        return str(result)
+
+
