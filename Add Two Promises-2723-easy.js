77c3319c5c7c0425071283b12832ed0c5f6164ad
Nozibul <nozibulislamspi@gmail.com>
2023-11-05 10:34:31 +0600

    Reverse string-344

--- Add Two Promises-2723-easy.js
+++ Add Two Promises-2723-easy.js
@@ -7,3 +7,12 @@ var addTwoPromises = async function (promise1, promise2) {
 };
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(3)).then(console.log); // 4
+
+// function sum(f, s, ){
+//   console.log("First:",f);
+//   console.log("Second:",s);
+//   // console.log("Third:",t);
+// }
+// const n ="islam";
+// const age = 54;
+// sum`${n} are is ${age}`
---
+++ Reverse string-344-easy.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse()
+};
+const result = reverseString(["h","e","l","l","o"])
+console.log(result);
