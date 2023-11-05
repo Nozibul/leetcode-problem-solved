084bd061072047033acb8f305241922a9a6a02a9
Nozibul <nozibulislamspi@gmail.com>
2023-11-05 21:24:12 +0600

    find the index first occurrence in a string -28

---
+++ Add string-415-easy.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    console.log(parseInt(typeof num2));
+};
+const result = addStrings("11", "123")
---
+++ find index of the first occurrence in a string-28-easy.js
@@ -0,0 +1,6 @@
+
+  var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+  };
+  const result = strStr("leetcode", "leeto");
+  console.log(result); // -1
