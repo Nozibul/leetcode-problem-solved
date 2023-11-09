a17ab3303ce71614dc02424a6c86d25b725dde64
Nozibul <nozibulislamspi@gmail.com>
2023-11-09 19:42:22 +0600

    Repeated Substring Pattern-459

---
+++ Repeated substring pattern-459-easy.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+ let reg = /^(\w+)\1+$/
+  return reg.test(s)
+};
+const result = repeatedSubstringPattern("abcabcabcabc");
+console.log(result);
