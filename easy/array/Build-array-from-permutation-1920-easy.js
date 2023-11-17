0676745977b4750eae47f14b7e8af2c4c86bcfed
Nozibul <nozibulislamspi@gmail.com>
2023-11-17 15:24:15 +0600

    Build array from permutation 1920

--- easy/array/Build-array-from-permutation-1920-easy.js
+++ easy/array/Build-array-from-permutation-1920-easy.js
@@ -3,5 +3,13 @@
  * @return {number[]}
  */
 var buildArray = function(nums) {
+    let newStr = [];
+
+    for (let i = 0; i < nums.length; i++){
+        newStr.push(nums[nums[i]]);
     
+    }
+    return newStr;
 };
+const result = buildArray([0,2,1,5,3,4]);
+console.log(result);
