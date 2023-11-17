aed4e9eea06473d093327f21cbeb79475c768f53
Nozibul <nozibulislamspi@gmail.com>
2023-11-17 12:36:07 +0600

    Update the folder structure of problem category

---
+++ easy/string/Find the difference-389-easy.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+// var findTheDifference = function(s, t) {
+//     for (let i = 0; i < t.length; i++) {
+//         if(!(t[i] === s[i])) return t[i];
+//     }
+// };
+
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let ret = 0; 
+    
+    for (let i = 0; i < s.length; i++) {
+        ret ^= s.charCodeAt(i); // XOR the character codes of string s, binary sum
+    };
+    for (let i = 0; i < t.length; i++) {
+        ret ^= t.charCodeAt(i); // XOR the character codes of string t
+    };
+    
+    return String.fromCharCode(ret); // Convert the XOR result to a character
+};
+const result = findTheDifference("ABC", "abcde");
+console.log(result);
