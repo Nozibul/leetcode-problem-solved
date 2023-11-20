8c8a1af802fb32d50c5e194ef0895fd238edde63
Nozibul <nozibulislamspi@gmail.com>
2023-11-20 08:36:29 +0600

    Shuffle the array 1470

---
+++ easy/array/Shuffle-the-array-1470-easy.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let arr=[];
+
+    for(let i = 0;i< n;i++){
+        arr.push(nums[i],nums[n+i]);
+    }
+    return arr;
+};
+const result = shuffle([2,5,1,3,4,7], 3);
+console.log(result); 
---
+++ easy/array/final-value-of-variable-after-performing-operations-2011-easy.js
@@ -0,0 +1,19 @@
+// /**
+//  * @param {string[]} operations
+//  * @return {number}
+//  */
+// var finalValueAfterOperations = function(operations) {
+//     let num = 0 ;
+
+//     for(let operation of operations){
+//         if(operation === "--X" || operation === "X--") num-- ;
+//         else if(operation === "++X" || operation === "X++") num++ ;
+//         else 0 ;
+//     };
+//     return num;
+// };
+// const result = finalValueAfterOperations(["++X","++X","X++"]);
+// console.log(result);
+
+
+
