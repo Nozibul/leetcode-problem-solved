27185f810227544f8638f6b3822fb8b85a0d80f1
Nozibul <nozibulislamspi@gmail.com>
2023-11-29 19:37:35 +0600

    Difference Between Element Sum and Digit Sum of an Array 2535

--- easy/array/Difference Between Element Sum and Digit Sum of an Array-2535-easy.js
+++ easy/array/Difference Between Element Sum and Digit Sum of an Array-2535-easy.js
@@ -3,11 +3,20 @@
  * @return {number}
  */
 var differenceOfSum = function(nums) {
-    const [ res, res2]= nums.reduce((acc, curr)=>{
-      console.log( curr);
-      return acc + curr ;
-    }, [0 ,0])
-  console.log(res2);
+   let  digitsSum = 0 ;
+    const sum = nums.reduce((acc, curr)=> acc + curr ,0); // 25
+
+    let digitSum = nums.reduce(function (acc, value) {
+      digitsSum = 0;
+
+      while(value){
+        digitsSum += value % 10;
+          value = Math.floor(value / 10);
+      }
+      return acc + digitsSum
+  }, 0);
+
+  return Math.abs(sum - digitSum);
 };
 const result = differenceOfSum([1,15,6,3]);
 console.log(result);
