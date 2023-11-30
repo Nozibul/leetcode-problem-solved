649ee5b530436d91569894f733b28ac674a22321
Nozibul <nozibulislamspi@gmail.com>
2023-11-30 20:31:38 +0600

    Check if a String Is an Acronym of Words 2828

---
+++ easy/array/Check if a string is an Acronym of words-2828-easy.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
+    let acronym = "";
+    for (let i = 0; i < words.length; i++) {
+       acronym +=words[i].charAt(0);        
+    }
+    if(acronym === s) return true ;
+    return false;
+};
+const result = isAcronym(["an","apple"], "a");
+console.log(result); // false
