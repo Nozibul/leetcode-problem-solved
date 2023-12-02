56c71af95aa08f32ac6e4a4e613e49695b74186c
Nozibul <nozibulislamspi@gmail.com>
2023-12-02 23:44:39 +0600

    Update Maximum sum with exactly K element -2656

--- easy/array/Maximum sum with exactly K elements-2656-easy.js
+++ easy/array/Maximum sum with exactly K elements-2656-easy.js
@@ -16,4 +16,12 @@ var maximizeSum = function(nums, k) {
     return totalMax;
 };
 const result = maximizeSum([1,2,3,4,5], 3);
-console.log(result);
+console.log(result); // 18
+
+
+// another approach
+var maximizeSum = function(nums, k) {
+    const max = Math.max(...nums)
+
+    return (k * (max + max + k - 1)) / 2
+};
