1442954f8653ff0ff049d6284fd70f91e7b5915d
Nozibul <nozibulislamspi@gmail.com>
2023-12-18 10:19:58 +0600

    Maximum Product Difference Between Two Pairs-1913

---
+++ easy/array/Maximum Product Difference Between Two Pairs-1913-easy.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    nums.sort((a, b)=> a-b);
+
+    return (nums[nums.length-2]*nums[nums.length-1]) - (nums[1]*nums[0])
+};
+const result = maxProductDifference([1,6,7,5,2,4,10,6,4]);
+console.log(result); // 68
