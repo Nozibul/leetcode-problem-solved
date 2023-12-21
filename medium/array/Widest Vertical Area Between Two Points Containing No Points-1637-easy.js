f576cc061a3dfa2e0a2a32fbc923f8d3e0d3492a
Nozibul <nozibulislamspi@gmail.com>
2023-12-21 19:37:29 +0600

    Widest Vertical Area Between Two Points Containing No Points-1637

---
+++ medium/array/Widest Vertical Area Between Two Points Containing No Points-1637-easy.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function(points) {
+    points.sort((a, b) => a[0] - b[0]);
+
+    let maxWidth = 0;
+
+    for (let i = 1; i < points.length; i++) {
+        let width = points[i][0] - points[i - 1][0];
+        maxWidth = Math.max(maxWidth, width);
+    }
+
+    return maxWidth;
+};
+const result = maxWidthOfVerticalArea([[8,7],[9,9],[7,4],[9,7]])
+console.log(result); // 1
