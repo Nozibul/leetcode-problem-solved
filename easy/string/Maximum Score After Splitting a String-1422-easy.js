366494d575ad5c27b3a7a2be4b6bbf51db79f78f
Nozibul <nozibulislamspi@gmail.com>
2023-12-22 14:37:20 +0600

    Maximum Score After Splitting a String-1422

---
+++ easy/string/Maximum Score After Splitting a String-1422-easy.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var maxScore = function(s) {
+    let left = 0;
+    let right = (s.match(/1/g) || []).length;
+    // console.log(right); // 4
+   
+    let max = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        s[i] === '0' ? left++ : right--;
+        max = Math.max(max, left + right);
+    }
+    return max;
+};
+const result = maxScore("1111");
+console.log(result); // 5
+
+//const count = (s.match(/1/g) || []).length;
