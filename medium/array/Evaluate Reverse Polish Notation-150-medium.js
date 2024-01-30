8fd9c76ad7b4b46e5364473daa3effb0b4000980
Nozibul <nozibulislamspi@gmail.com>
2024-01-30 22:38:53 +0600

    Evaluate Reverse Polish Notation 150

---
+++ medium/array/Evaluate Reverse Polish Notation-150-medium.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack = [];
+    for (let i = 0; i < tokens.length; i++) {
+        switch (tokens[i]) {
+            case "+":
+                stack[stack.length - 2] += stack.pop();
+                break;
+            case "-":
+                stack[stack.length - 2] -= stack.pop();
+                break;
+            case "*":
+                stack[stack.length - 2] *= stack.pop();
+                break;
+            case "/":
+                stack[stack.length - 2] = Math.floor((stack[stack.length - 2] / stack.pop()) | 0);
+                break;
+            default:
+                stack.push(Number(tokens[i]));
+                break;
+        }
+    }
+    return stack[0];
+};
+console.log(evalRPN(["2","1","+","3","*"]));
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
