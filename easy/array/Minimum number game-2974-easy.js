0fd58bcf6657467932237e51cece42974931bc33
Nozibul <nozibulislamspi@gmail.com>
2024-02-02 17:35:42 +0600

    Minimum Number game 2974

---
+++ easy/array/Minimum number game-2974-easy.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums){
+    nums.sort((a, b) => a - b);
+
+    const newArr = [];
+
+    for(let i = 0; i < nums.length-1; i+=2){
+        newArr.push(nums[i+1], nums[i]);
+    }
+    return newArr;
+};
+
+console.log(numberGame([5,4,2,3])); // [3,2,5,4]
