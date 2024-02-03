06148bf0f2c43a25e58423fb22f90befb7dab914
Nozibul <nozibulislamspi@gmail.com>
2024-02-03 16:32:49 +0600

    Count Number of Pairs With Absolute Difference K 2006

---
+++ easy/array/Count Number of Pairs With Absolute Difference K 2006-easy.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function (nums, k) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) == k) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+console.log([1, 2, 2, 1], 1); // 4
+/*
+Explanation: The pairs with an absolute difference of 1 are:
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1] 
+*/
