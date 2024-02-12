72a93a4332aacf6ad29414e94c7479a274037e74
Nozibul <nozibulislamspi@gmail.com>
2024-02-12 22:21:05 +0600

    Majority Element 169

---
+++ easy/Hashtable/Majority Element-169-easy.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const counts = {};
+    
+    for (let num of nums) {  
+        counts[num] = (counts[num] || 0) + 1;
+        if (counts[num] >= nums.length / 2) return num;
+    }
+};
+console.log(majorityElement([3,2,2,1,1,1,2,2])); // Output: 2
+
+
+/* 
+ var majorityElement = function(nums) {
+    let map = {}
+    let n = nums.length;
+    
+    for(let i of nums){
+        if(!map[i]) {
+            map[i] = 1;
+        } else {
+            map[i]++;
+        }
+        if (map[i] >= n/2) return i
+    }
+    console.log(map)
+ };
+ */
