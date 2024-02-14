3ad5c72de80b586694f7f3757d35a0056f7ed816
Nozibul <nozibulislamspi@gmail.com>
2024-02-14 22:19:56 +0600

    Number of Arithmetic Triplets 2367

---
+++ easy/Hashtable/Number of Arithmetic Triplets-2367-easy.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    
+    let hash = new Map();
+    let count = 0;
+    
+    for(let num of nums){
+        let temp = num - diff;
+        
+        if(hash.has(temp) && hash.has(temp - diff)){
+            count++;
+        }
+        hash.set(num);
+    }
+    console.log(hash);
+    return count;
+};
+console.log(arithmeticTriplets([4,5,6,7,8,9], 2)); // 2
