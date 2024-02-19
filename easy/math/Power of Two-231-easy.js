4c9d4675db1d561097cb949235b0519f478c1934
Nozibul <nozibulislamspi@gmail.com>
2024-02-19 12:15:23 +0600

    Power of Two 231

---
+++ easy/math/Power of Two-231-easy.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    return (Math.log2(n) % 1 === 0);
+};
+console.log(isPowerOfTwo(16)) //  true
+
+
+// another solution 
+/* 
+    var isPowerOfTwo = function(n) {
+        return Number.isInteger(Math.log2(n));
+    }; 
+*/
