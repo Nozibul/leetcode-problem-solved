4e4b797298181645e423bf14920972a678b10b10
Nozibul <nozibulislamspi@gmail.com>
2024-02-27 17:40:47 +0600

    Subtree of Another Tree 527

---
+++ Intermediate/Tree/Depth-First Search-Binary tree/Subtree of Another Tree-572-easy.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+const isSameTree = (a, b)=>{
+    if (!a && !b) return true;
+    if (a && !b) return false;
+    if (b && !a) return false;
+    if (a.val === b.val) return isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
+}
+
+var isSubtree = function(root, subRoot) {
+    if (!root) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
+
+console.log(isSubtree([3,4,5,1,2,null,null,null,null,0], [4,1,2])); // true
