65733ffd982a691381071bd7df10df18fa60c326
Nozibul <nozibulislamspi@gmail.com>
2024-03-02 17:12:51 +0600

    Squares of a Sorted Array 977

---
+++ Fundamental/Sorting/Squares of a Sorted Array-977-easy.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] * nums[i];
+    }
+
+    return nums.sort((a, b) => a - b);
+};
+
+console.log(sortedSquares([-4,-1,0,3,10])); //[0,1,9,16,100]
