c7587193e909ddc7ae6fc623cc7c1b912d561a3b
Nozibul <nozibulislamspi@gmail.com>
2024-03-13 12:55:02 +0600

    Find the Pivot Integer 2485

---
+++ Intermediate/Math/easy/Find the Pivot Integer-2485-easy.js
@@ -0,0 +1,10 @@
+var pivotInteger = function(n) {
+    let x = Math.sqrt(n * (n + 1) / 2);
+    
+    if (x % 1 !== 0) {
+        return -1;
+    } else {
+        return Math.floor(x);
+    }
+};
+console.log(pivotInteger(8)); //6
