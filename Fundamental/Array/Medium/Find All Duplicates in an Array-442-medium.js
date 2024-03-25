fcaf3128b4cbdef3af631014d53b0d7075b34491
Nozibul <nozibulislamspi@gmail.com>
2024-03-25 21:45:45 +0600

    Find All Duplicates in an Array 442

---
+++ Fundamental/Array/Medium/Find All Duplicates in an Array-442-medium.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        const num = Math.abs(nums[i]);
+        const idx = num - 1;
+        if (nums[idx] < 0)
+            result.push(num);
+        nums[idx] *= -1;
+    }
+    return result;
+};
+console.log(findDuplicates([4,3,2,7,8,2,3,1] )); //[4,3,2,7,8,2,3,1]
