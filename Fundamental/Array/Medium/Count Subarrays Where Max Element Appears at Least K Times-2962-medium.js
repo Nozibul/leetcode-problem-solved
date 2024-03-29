8f851730dd5c7dfafe338e781c8ee59a001f4e9d
Nozibul <nozibulislamspi@gmail.com>
2024-03-29 07:34:21 +0600

    Count Subarrays Where Max Element Appears at Least K Times 2962

---
+++ Fundamental/Array/Medium/Count Subarrays Where Max Element Appears at Least K Times-2962-medium.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countSubarrays = function (nums, k) {
+    let ans = 0;
+    let start = 0;
+    let count = 0;
+    const max = Math.max(...nums);
+  
+    for (let end = 0; end < nums.length; end++) {
+      if (nums[end] === max) count++;
+      while (count === k) {
+        ans += nums.length - end;
+        if (nums[start] === max) count--;
+        start++;
+      }
+    }
+  
+    return ans;
+  };
+  console.log(countSubarrays([1,3,2,3,3], 2)); // 6
