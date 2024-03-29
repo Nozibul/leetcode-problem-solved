3512f83bf5d7bb48c63cc75d2ddc2efa629f9201
Nozibul <nozibulislamspi@gmail.com>
2024-03-29 07:28:08 +0600

    Length of Longest Subarray With at Most K Frequency 2958

---
+++ Intermediate/Hash Table/medium/Length of Longest Subarray With at Most K Frequency-2958.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxSubarrayLength = function(nums, k) {
+    const freq = {};
+    const n = nums.length;
+    let ans = 0;
+    let i = 0;
+    let j = 0;
+    while (j < n) {
+        freq[nums[j]] = (freq[nums[j]] || 0) + 1;
+        while (freq[nums[j]] > k) {
+            freq[nums[i]]--;
+            i++;
+        }
+        ans = Math.max(ans, j - i + 1);
+        j++;
+    }
+    return ans;
+};
+
+console.log(maxSubarrayLength([1,2,3,1,2,3,1,2], 6)); // 6
