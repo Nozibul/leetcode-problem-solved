72cc768c984d53f2bf06541ee5c185b76de6f7ee
Nozibul <nozibulislamspi@gmail.com>
2024-04-03 20:58:51 +0600

    Minimum Sum of Four Digit Number After Splitting Digits 2160

--- Fundamental/Sorting/Minimum Number of Arrows to Burst Balloons-452-medium.js
+++ Fundamental/Sorting/Minimum Number of Arrows to Burst Balloons-452-medium.js
@@ -8,15 +8,11 @@ var findMinArrowShots = function(points) {
     let arrows = 1;
     let end = points[0][1];
 
-    
-
     for (let i = 1; i < points.length; i++) {
         if (points[i][0] > end) {
             arrows++;
             end = points[i][1];
-        } else {
-            end = Math.min(end, points[i][1]);
-        }
+        } else end = Math.min(end, points[i][1]);
     }
 
     return arrows;
