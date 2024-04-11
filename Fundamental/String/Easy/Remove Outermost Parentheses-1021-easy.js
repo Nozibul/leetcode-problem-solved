79344b100161bd07fbb2fa3c05c58c33991b9bd9
Nozibul <nozibulislamspi@gmail.com>
2024-04-11 11:35:24 +0600

    Remove Outermost Parentheses 1021

---
+++ Fundamental/String/Easy/Remove Outermost Parentheses-1021-easy.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function (s) {
+    let count = 0, outer = ""
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            count++;
+        }
+        if (count > 1) {
+            outer += s[i]
+        }
+        if (s[i] === ")") {
+            count--;
+        }
+    }
+    return outer;
+};
+console.log(removeOuterParentheses("(()())(())")); //"()()()"
