b0ec40a1f8359ed21db22ae40b59a23b23456f4d
Nozibul <nozibulislamspi@gmail.com>
2024-04-13 15:00:52 +0600

    Number of Changing Keys 3019

---
+++ Fundamental/String/Easy/Number of Changing Keys-3019-easy.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countKeyChanges = function(s) {
+    let arr = s.toUpperCase().split('');
+    let count = 0;
+
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] !== arr[i+1])
+            count++;
+    }
+    
+    return(count - 1); 
+};
+
+console.log(countKeyChanges("aAbBcC")); // 2 
