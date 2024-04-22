bafc4e42f63841c5e975ed97e1d2dac849321a54
Nozibul <nozibulislamspi@gmail.com>
2024-04-22 12:11:27 +0600

    Partitioning Into Minimum Number Of Deci-Binary Numbers 1689

---
+++ Fundamental/String/Medium/Partitioning Into Minimum Number Of Deci-Binary Numbers-1689-medium.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} n
+ * @return {number}
+ */
+const minPartitions = n => Math.max(console.log(...n));
+
+console.log(minPartitions("82734")); //  8
+console.log(minPartitions("32")); //Explanation: 10 + 11 + 11 = 32
