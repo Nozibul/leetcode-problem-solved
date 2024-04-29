db70fc220d874e3da593ab899542569d17e7b546
Nozibul <nozibulislamspi@gmail.com>
2024-04-29 11:44:00 +0600

    Sort Vowels in a String 2785

---
+++ Intermediate/Hash Table/medium/Sort Vowels in a String-2785-medium.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortVowels = function(s) {
+    let hash = new Map([['A', 1],['E', 1],['I', 1],['O', 1],['U', 1],['a', 1],['e', 1],['i', 1],['o', 1],['u', 1]]);
+   let idsave = [];
+   let vowsave = [];
+    for(let i=0; i<s.length; i++) {
+        if(hash.has(s[i])) {
+            idsave.push(i);
+            vowsave.push(s[i]);
+        }
+    }
+    vowsave.sort();
+    let res = s.split('');
+    for(let i=0; i<idsave.length; i++) {
+        res[idsave[i]] = vowsave[i];
+    }
+    return res.join('');
+};
