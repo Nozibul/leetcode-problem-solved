166d3d185e8f5d4f09fb4b35407d2bc62fa2c2e9
Nozibul <nozibulislamspi@gmail.com>
2024-07-12 19:56:11 +0600

    Maximum Score From Removing Substrings 1717

--- Intermediate/Greedy/medium/Minimum Difference Between Largest and Smallest Value in Three Moves-1509.js
+++ Intermediate/Greedy/medium/Minimum Difference Between Largest and Smallest Value in Three Moves-1509.js
@@ -7,11 +7,6 @@ var minDifference = function(nums) {
     if (n <= 4) return 0; 
     nums.sort((a, b) => a - b); 
     
-    // Consider the four possible cases:
-    // 1. Change the largest three elements
-    // 2. Change the smallest element and the largest two elements
-    // 3. Change the two smallest elements and the largest element
-    // 4. Change the three smallest elements
     return Math.min(
         nums[n - 1] - nums[3],
         nums[n - 2] - nums[2],
