1b1886ac26915b025b82fdc06a02eb1a310e8e2d
Nozibul <nozibulislamspi@gmail.com>
2024-09-06 07:44:23 +0600

    Delete Nodes From Linked List Present in Array 3217

---
+++ Fundamental/Linked List/Delete Nodes From Linked List Present in Array-3217.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} excludeValues
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(excludeValues, head) {
+    const excludeSet = new Set(excludeValues);
+    
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let curr = dummy;
+    
+    while (curr.next) {
+        if (excludeSet.has(curr.next.val)) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;  
+        }
+    }
+    
+    return dummy.next;
+};
+
+console.log(modifiedList([2,3,4], [1,2,3,4,5,6])); // [1,5,6]
+console.log(modifiedList([1], [1,2,1,2,1,2])); // [2,2,2
