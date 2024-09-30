17ddfe121516cc7ff89f953503b40a62d2af1265
Nozibul <nozibulislamspi@gmail.com>
2024-09-30 07:44:10 +0600

    Ransom Note 383

---
+++ Fundamental/String/Easy/Ransom Note-383.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+    for (const char of magazine) {
+      ransomNote = ransomNote.replace(char, "");
+    }
+    
+    if (!ransomNote) return true;
+    else return false;
+  };
+console.log(canConstruct("a", "b")); // false
