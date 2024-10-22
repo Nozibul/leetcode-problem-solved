2b2c50d7741e6c316b4a5ada8261a78e845e1ee9
Nozibul <nozibulislamspi@gmail.com>
2024-10-22 11:22:30 +0600

    Contains Duplicate 217

---
+++ Fundamental/Sorting/Contains Duplicate-217.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            return true;
+        }
+    }
+    
+    return false;    
+};
+console.log(containsDuplicate([1,2,3,1])); // true
