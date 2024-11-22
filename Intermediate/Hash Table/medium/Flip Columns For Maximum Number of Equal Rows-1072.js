dee4d079e236d88300a3e785740a234ec66db3ac
Nozibul <nozibulislamspi@gmail.com>
2024-11-22 11:47:08 +0600

    Flip Columns For Maximum Number of Equal Rows 1072

---
+++ Intermediate/Hash Table/medium/Flip Columns For Maximum Number of Equal Rows-1072.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxEqualRowsAfterFlips = function(matrix) {
+    d = {};
+    
+    for (let row of matrix) {
+        h = row[0]? row.join("") : row.map(x => 1 - x).join("")
+        d[h] = (d[h] || 0) + 1
+    }
+    return Math.max(...Object.values(d))
+};
+console.log(maxEqualRowsAfterFlips([[0,1],[1,1]])); // 1
