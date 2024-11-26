03373655e7c0af2d3722d0b34891997a9ee0f5ff
Nozibul <nozibulislamspi@gmail.com>
2024-11-26 11:44:38 +0600

    Find Champion II 2924

---
+++ Advanced/graph/Find Champion II-2924.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+const findChampion = function (n, edges) {
+    const ind = {};
+    for (const [u, v] of edges) {
+      ind[v] = ind[v] || 0;
+      ind[v]++;
+    }
+  
+    let ans = [];
+    for (let u = 0; u < n; u++) {
+      if (ind[u] === undefined) {
+        ans.push(u)
+      }
+    }
+    return (ans.length === 1) ? ans[0] : -1
+  };
+  console.log(findChampion(3, [[0,1],[1,2]])); // 0
