e94f0f1a22c15750cf6b0298506dc8e9dd1b99f6
Nozibul <nozibulislamspi@gmail.com>
2024-12-11 21:13:21 +0600

    Third Maximum Number 414

---
+++ Fundamental/Sorting/Third Maximum Number-414.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const thirdMax = function (nums) {
+  let set = new Set(nums);
+  const myArray = Array.from(set);
+  return myArray.sort((a, b) => b - a).length < 3 ? myArray[0] : myArray[2];
+};
+console.log(thirdMax([3,2,1])); // 1
