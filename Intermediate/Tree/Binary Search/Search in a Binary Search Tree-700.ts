45dcf78066da35fa4276597492a5b594dec4b26c
Nozibul <nozibulislamspi@gmail.com>
2025-01-01 16:06:52 +0600

    Search in a Binary Search Tree 700

---
+++ Intermediate/Tree/Binary Search/Search in a Binary Search Tree-700.ts
@@ -0,0 +1,18 @@
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+const searchBST = function(root, val) {
+    if (!root) {
+        return null;
+    }
+    if (root.val === val) {
+        return root;
+    } else if (root.val > val) {
+        return searchBST(root.left, val);
+    } else {
+        return searchBST(root.right, val);
+    }
+};
+    
