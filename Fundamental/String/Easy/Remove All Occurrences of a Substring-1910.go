5fd3c2ecbb2dd8ba3eb1da2209299ee508e899b0
Nozibul <nozibulislamspi@gmail.com>
2025-02-11 23:53:31 +0600

    package main

---
+++ Fundamental/String/Easy/Remove All Occurrences of a Substring-1910.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// removeOccurrences removes all occurrences of 'part' from 's'
+func removeOccurrences(s string, part string) string {
+	idx := strings.Index(s, part)
+	for idx != -1 {
+		s = s[:idx] + s[idx+len(part):]
+		idx = strings.Index(s, part)
+	}
+	return s
+}
+
+func main() {
+	// Example usage of removeOccurrences
+	s := "hellohellohello"
+	part := "hello"
+	result := removeOccurrences(s, part)
+	fmt.Println("Original String:", s)
+	fmt.Println("Modified String:", result)
+}
