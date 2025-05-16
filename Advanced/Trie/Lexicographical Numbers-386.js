48a254c8d15a23b6c4d3f5ed81df5c0177338af1
Nozibul <nozibulislamspi@gmail.com>
2025-05-16 15:48:50 +0600

    Lexicographical Numbers

--- Advanced/Trie/Lexicographical Numbers-386.js
+++ Advanced/Trie/Lexicographical Numbers-386.js
@@ -15,3 +15,4 @@ var lexicalOrder = function(n) {
     return res;
 };
 console.log(lexicalOrder(13)); // [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(lexicalOrder(11)); // [1, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9]
