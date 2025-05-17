a31c75d73af5bb81652c9dacc10ea3230aca3e00
Nozibul <nozibulislamspi@gmail.com>
2025-05-17 18:05:01 +0600

    Count Subarrays With Fixed Bounds

--- Advanced/Monotonic Stack/Count Subarrays With Fixed Bounds-2444-hard.js
+++ Advanced/Monotonic Stack/Count Subarrays With Fixed Bounds-2444-hard.js
@@ -4,27 +4,28 @@
  * @param {number} maxK
  * @return {number}
  */
-var countSubarrays = function(nums, minK, maxK) {
-        const n = nums.length;
-        let result = 0;
-        let minKIndex = -1;
-        let maxKIndex = -1;
-        let culpritIndex = -1;
+var countSubarrays = function (nums, minK, maxK) {
+  const n = nums.length;
+  let result = 0;
+  let minKIndex = -1;
+  let maxKIndex = -1;
+  let culpritIndex = -1;
 
-        for (let i = 0; i < n; i++) {
-            if (nums[i] < minK || nums[i] > maxK) culpritIndex = i;
-            
-            if (nums[i] === minK) minKIndex = i;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] < minK || nums[i] > maxK) culpritIndex = i;
 
-            if (nums[i] === maxK) maxKIndex = i;
+    if (nums[i] === minK) minKIndex = i;
 
-            const smaller = Math.min(minKIndex, maxKIndex);
-            const temp = smaller - culpritIndex;
+    if (nums[i] === maxK) maxKIndex = i;
 
-            result += temp <= 0 ? 0 : temp;
-        }
-        
-        return result;
+    const smaller = Math.min(minKIndex, maxKIndex);
+    const temp = smaller - culpritIndex;
+
+    result += temp <= 0 ? 0 : temp;
+  }
+
+  return result;
 };
 
-console.log(countSubarrays([1,3,5,2,7,5], 1, 5)); // 2
+console.log(countSubarrays([1, 3, 5, 2, 7, 5], 1, 5)); // 2
+console.log(countSubarrays([1, 2, 2, 3, 4, 5, 6], 3, 2)); // 3
