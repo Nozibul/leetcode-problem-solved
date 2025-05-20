6a55ac7ce3421d9aea863de1f7405da2c382da22
Nozibul <nozibulislamspi@gmail.com>
2025-05-20 17:15:28 +0600

    nim-game-29

--- Advanced/Game Theory/nim-game-29-easy.js
+++ Advanced/Game Theory/nim-game-29-easy.js
@@ -1,9 +1,6 @@
-var canWinNim = function(n) {
-console.log(n%4);
-  if(n % 4 !== 0){
-        return true
-   }else{
-    return false
-   }
+var canWinNim = function (n) {
+  if (n % 4 !== 0) return true;
+  return false;
 };
-console.log(canWinNim(5));
+console.log(canWinNim(5)); // true
+console.log(canWinNim(8)); // false
