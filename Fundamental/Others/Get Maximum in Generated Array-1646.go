c6ebcf5960af9ec88a90af2798b7f87f4c6de321
Nozibul <nozibulislamspi@gmail.com>
2025-05-31 20:01:11 +0600

    Get Maximum in Generated Array-1646

--- Fundamental/Others/Get Maximum in Generated Array-1646.go
+++ Fundamental/Others/Get Maximum in Generated Array-1646.go
@@ -1,3 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func getMaximumGenerated(n int) int {
 	if n < 2 {
 		return n
@@ -23,4 +31,29 @@ func getMaximumGenerated(n int) int {
 	}
 
 	return max
+}
+
+func main() {
+	// Test with different values
+	testValues := []int{0, 1, 2, 3, 4, 7, 15, 20}
+
+	fmt.Println("Testing getMaximumGenerated function:")
+	fmt.Println("=====================================")
+
+	for _, n := range testValues {
+		result := getMaximumGenerated(n)
+		fmt.Printf("n = %2d: maximum = %d\n", n, result)
+	}
+
+	// Interactive mode - accept command line argument
+	if len(os.Args) > 1 {
+		if input, err := strconv.Atoi(os.Args[1]); err == nil {
+			fmt.Printf("\nCommand line input n = %d: maximum = %d\n", input, getMaximumGenerated(input))
+		} else {
+			fmt.Printf("Invalid input: %s. Please provide a valid integer.\n", os.Args[1])
+		}
+	}
+
+	fmt.Println("\nUsage: go run main.go [number]")
+	fmt.Println("Example: go run main.go 15")
 }
