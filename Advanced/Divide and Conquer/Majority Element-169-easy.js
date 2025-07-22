1f676b419caef2d1ecbb53b098adaf020ad1d911
Nozibul <nozibulislamspi@gmail.com>
2025-07-22 23:12:42 +0600

    Majority Element-169-easy

--- Advanced/Divide and Conquer/Majority Element-169-easy.js
+++ Advanced/Divide and Conquer/Majority Element-169-easy.js
@@ -14,19 +14,3 @@ console.log(majorityElement([3,2,2,1,1,1,2,2])); // Output: 2
 console.log(majorityElement([2, 4, 5, 6, 8, 1, 2])); // Output: 2
 
 
-/* 
- var majorityElement = function(nums) {
-    let map = {}
-    let n = nums.length;
-    
-    for(let i of nums){
-        if(!map[i]) {
-            map[i] = 1;
-        } else {
-            map[i]++;
-        }
-        if (map[i] >= n/2) return i
-    }
-    console.log(map)
- };
- */
