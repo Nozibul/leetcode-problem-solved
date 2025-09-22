4b74d9d0689156cdd137e0a1743c2fbc156944e4
Nozibul <nozibulislamspi@gmail.com>
2025-09-22 19:00:06 +0600

    Reverse Only Letters

---
+++ Two-pointer/Easy/Reverse Only Letters.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var reverseOnlyLetters = function(s) {
+    //Edge case
+    if (s.length === 0) return s;
+
+    //Initialize variables with meaningful names
+    let left = 0, right = s.length - 1;
+    let newValue = s.split("");
+
+    //Two pointer approach
+    while (left <= right) {
+        if (/[a-zA-Z]/.test(newValue[left]) && /[a-zA-Z]/.test(newValue[right])) {
+            [newValue[left], newValue[right]] = [newValue[right], newValue[left]]; // swap
+            left++;
+            right--;
+        } else if (!/[a-zA-Z]/.test(newValue[left])) {
+            left++;
+        } else if (!/[a-zA-Z]/.test(newValue[right])) {
+            right--;
+        }
+    }
+    // eturn statement
+    return newValue.join("");
+};
+
+console.log(reverseOnlyLetters("ab-cd")); // "dc-ba"
+console.log(reverseOnlyLetters("a-bC-dEf-ghIj")); // "j-Ih-gfE-dCba"
