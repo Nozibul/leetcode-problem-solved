d267d4f51c00f540da8bf343098790ed40d9a093
Nozibul <nozibulislamspi@gmail.com>
2025-09-28 12:14:13 +0600

    Happy Number

---
+++ Two-pointer/Easy/Happy Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    // Edge case
+    if (n <= 0 || n === 1) return false;
+
+    while (true) {
+        n = String(n)
+            .split('')
+            .reduce((acc, digit) => acc + Math.pow(Number(digit), 2), 0);
+        if (n < 10) {
+            return n === 1;
+        }
+    }
+};
+console.log(isHappy(19)) // true
